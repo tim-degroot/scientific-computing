0f2d5d7d90aea1c6ae52ad05af1b02a9752caa47
lnca-dotcom <lucavandernooij@gmail.com>
2026-02-22 02:01:56 +0100

    Functions now contained in a class, clearer plots

--- Set-1/Jacobi_SOR.py
+++ Set-1/Jacobi_SOR.py
@@ -7,12 +7,12 @@ import matplotlib.pyplot as plt
 # H
 # ========================================
 
-def Jacobi_Iteration(N = 50, epsilon=1e-5):
+def Jacobi_Iteration(N = 50, epsilon=1e-5, max_iter=10000):
     c = np.zeros((N+1,N+1))
     c[0,:] = np.ones(N+1)
     c_new = np.copy(c)
     it = 0
-    while True:
+    while it<max_iter:
         for i in range(N+1):
             for j in range(1, N):
                 c_new[j][i] = (1/4) * (c[j][(i+1) % (N+1)] + c[j][(i-1) % (N+1)] + c[j+1][i] + c[j-1][i])
@@ -24,7 +24,7 @@ def Jacobi_Iteration(N = 50, epsilon=1e-5):
     return c_new.round(2), it
 
 
-def SOR_Iteration(omega, N = 50, epsilon=1e-5, coor_size=[], insulation=None, max_iter=5000):
+def SOR_Iteration(omega, N = 50, epsilon=1e-5, coor_size=[], insulation=None, max_iter=10000):
     c = np.zeros((N+1,N+1))
     c[0,:] = np.ones(N+1)
     c_new = np.copy(c)
@@ -52,41 +52,43 @@ def SOR_Iteration(omega, N = 50, epsilon=1e-5, coor_size=[], insulation=None, ma
         c = np.copy(c_new)
     return c_new.round(2), it
 
-# print(Jacobi_Iteration(N=50))
-# print(SOR_Iteration(omega=1, N=50)) # Gauss_Seidel
-# print(SOR_Iteration(N=50))
-
-
 # ========================================
 # I
 # ========================================
 
-p_vals = list(range(11))
-omega_vals = [.5, 1.25, 1.5, 1.6, 1.7, 1.8, 1.9]
+p_vals = list(range(6))
+omega_vals = [.5, 1.5, 1.7, 1.8, 1.9] # 1.25, 1.6
 it_vals_jac = []
 it_vals_GS = []
-it_vals_SOR = [[], [], [], [], [], [], []]
+it_vals_SOR = [[], [], [], [], []]
 
-# for p in p_vals:
-#     c_jac, it_jac = Jacobi_Iteration(N=50, epsilon=10*np.e**(-p))
-#     c_GS, it_GS = SOR_Iteration(omega=1, N=50, epsilon=10*np.e**(-p))
-#     for w in range(len(omega_vals)):
-#         c_SOR, it_SOR = SOR_Iteration(omega=omega_vals[w], N=50, epsilon=10*np.e**(-p))
-#         it_vals_SOR[w].append(it_SOR)
+for p in p_vals:
+    print('p',p)
+    c_jac, it_jac = Jacobi_Iteration(N=50, epsilon=10**(-p))
+    print('jacobi:', it_jac)
+    c_GS, it_GS = SOR_Iteration(omega=1, N=50, epsilon=10**(-p))
+    print('GS:', it_GS)
+    for w in range(len(omega_vals)):
+        c_SOR, it_SOR = SOR_Iteration(omega=omega_vals[w], N=50, epsilon=10**(-p))
+        it_vals_SOR[w].append(it_SOR)
+        print('SOR, {}:'.format(omega_vals[w]), it_SOR)
 
-#     it_vals_jac.append(it_jac)
-#     it_vals_GS.append(it_GS)
+    it_vals_jac.append(it_jac)
+    it_vals_GS.append(it_GS)
 
-# fig, (ax1, ax2) = plt.subplots(1, 2)
-# ax1.semilogy(p_vals, it_vals_jac, marker='o', color='r', label='Jacobian')
-# ax1.semilogy(p_vals, it_vals_GS, marker='o', color='b', label='Gauss-Seidel')
-# ax1.semilogy(p_vals, it_vals_SOR[-2], marker='o', color='g', label=r'SOR, $\omega$=1.8')
-# for w in range(len(omega_vals)):
-#     ax2.semilogy(p_vals, it_vals_SOR[w], marker='o', label=r'$\omega$={}'.format(omega_vals[w]))
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(9,6), sharey=True)
+ax1.semilogy(p_vals, it_vals_jac, marker='o', color='g', label='Jacobian')
+ax1.semilogy(p_vals, it_vals_GS, marker='o', color='b', label='Gauss-Seidel')
+ax1.semilogy(p_vals, it_vals_SOR[-2], marker='o', color='r', label=r'SOR, $\omega$=1.8')
+for w in range(len(omega_vals)):
+    ax2.semilogy(p_vals, it_vals_SOR[w], marker='o', label=r'$\omega$={}'.format(omega_vals[w]))
 
-# ax1.legend()
-# ax2.legend(title=r"SOR $\omega$")
-# plt.show()
+ax1.legend()
+ax2.legend(title=r"SOR $\omega$")
+fig.supxlabel(r'$p\quad (\epsilon=10^{-p})$')
+fig.supylabel('number of iterations')
+plt.tight_layout()
+plt.show()
 
 
 # ========================================
@@ -101,7 +103,9 @@ def SOR_Optimize(omega, coor_size=[]):
 # op_omega = sp.optimize.minimize(SOR_Optimize, x0=1.8, bounds=[(1.7, 2.0)])
 # print(op_omega)
 
-omegas = np.linspace(1.7, 2.0-1e-6, 100)
+print('Finding optimal omega')
+omegas = np.linspace(1.8, 2.0, 100)
+omegas = omegas[:-1]
 # iters = [SOR_Optimize([w]) for w in omegas]
 
 # best_idx = np.argmin(iters)
@@ -113,19 +117,58 @@ omegas = np.linspace(1.7, 2.0-1e-6, 100)
 # K
 # ========================================
 
-c_0, it_0 = SOR_Iteration(1.8)
-print(it_0)
-plt.imshow(c_0, cmap='hot_r', interpolation='nearest')
+fig, ((ax1,ax2), (ax3,ax4)) = plt.subplots(2, 2, figsize=(6,6), sharey=True)
+
+c_0, it_0 = SOR_Iteration(1.9)
+print('no objects:', it_0)
+first = ax1.imshow(c_0, extent=[0, 1, 0, 1], cmap='plasma', interpolation='nearest')
+ax1.set_title('no objects')
+
+c_1, it_1 = SOR_Iteration(1.9, coor_size=[[5,20,10]])
+print('1 object, high:', it_1)
+ax2.imshow(c_1, extent=[0, 1, 0, 1], cmap='plasma', interpolation='nearest')
+ax2.set_title('1 object, high')
+
+c_2, it_2 = SOR_Iteration(1.9, coor_size=[[20,20,10]])
+print('1 object, middle:', it_2)
+ax3.imshow(c_2, extent=[0, 1, 0, 1], cmap='plasma', interpolation='nearest')
+ax3.set_title('1 object, middle')
+
+c_3, it_3 = SOR_Iteration(1.9, coor_size=[[35,20,10]])
+print('1 object, low:', it_3)
+ax4.imshow(c_3, extent=[0, 1, 0, 1], cmap='plasma', interpolation='nearest')
+ax4.set_title('1 object, low')
+
+fig.supxlabel('X')
+fig.supylabel('Y')
+fig.colorbar(first, ax=(ax1,ax2,ax3,ax4))
 plt.show()
 
-c_1, it_1 = SOR_Iteration(1.8, coor_size=[[20,20,10]])
-print(it_1)
-plt.imshow(c_1, cmap='hot_r', interpolation='nearest')
-plt.show()
+fig, ((ax1,ax2), (ax3,ax4)) = plt.subplots(2, 2, figsize=(6,6), sharey=True)
 
-c_2, it_2 = SOR_Iteration(1.8, coor_size=[[10,10,5], [30,30,5]])
-print(it_2)
-plt.imshow(c_2, cmap='hot_r', interpolation='nearest')
+c_7, it_7 = SOR_Iteration(1.9, coor_size=[[20,10,10], [20,30,10]])
+print('2 objects, horz:', it_7)
+first = ax1.imshow(c_7, extent=[0, 1, 0, 1], cmap='plasma', interpolation='nearest')
+ax1.set_title('2 objects, horizontal')
+
+c_8, it_8 = SOR_Iteration(1.9, coor_size=[[10,20,10], [30,20,10]])
+print('2 objects, vert:', it_8)
+ax2.imshow(c_8, extent=[0, 1, 0, 1], cmap='plasma', interpolation='nearest')
+ax2.set_title('2 objects, vertical')
+
+c_9, it_9 = SOR_Iteration(1.9, coor_size=[[10,10,10], [30,30,10]])
+print('2 objects, diag:', it_9)
+ax3.imshow(c_9, extent=[0, 1, 0, 1], cmap='plasma', interpolation='nearest')
+ax3.set_title('2 objects, diagonal')
+
+c_10, it_10 = SOR_Iteration(1.9, coor_size=[[10,10,10], [30,10,10], [10,30,10], [30,30,10]])
+print('4 objects, sqr:', it_10)
+ax4.imshow(c_10, extent=[0, 1, 0, 1], cmap='plasma', interpolation='nearest')
+ax4.set_title('4 objects, square')
+
+fig.supxlabel('X')
+fig.supylabel('Y')
+fig.colorbar(first, ax=(ax1,ax2, ax3,ax4))
 plt.show()
 
 # iters_1 = [SOR_Optimize([w], [[20,20,10]]) for w in omegas]
@@ -146,12 +189,34 @@ plt.show()
 # J
 # ========================================
 
-c_1, it_1 = SOR_Iteration(1.8, coor_size=[[20,20,10]], insulation=[0.9])
-print(it_1)
-plt.imshow(c_1, cmap='hot_r', interpolation='nearest')
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(6,6), sharey=True)
+
+c_3, it_3 = SOR_Iteration(1.9, coor_size=[[20,20,10]], insulation=[0.5])
+print('insulation = 0.5:', it_3)
+first = ax1.imshow(c_3, extent=[0, 1, 0, 1], cmap='plasma', interpolation='nearest')
+ax1.set_title('insulation = 0.5')
+
+c_4, it_4 = SOR_Iteration(1.9, coor_size=[[20,20,10]], insulation=[0.75])
+print('insulation = 0.75:', it_4)
+ax2.imshow(c_4, extent=[0, 1, 0, 1], cmap='plasma', interpolation='nearest')
+ax2.set_title('insulation = 0.75')
+
+c_5, it_5 = SOR_Iteration(1.9, coor_size=[[20,20,10]], insulation=[0.9])
+print('insulation = 0.9:', it_5)
+ax3.imshow(c_5, extent=[0, 1, 0, 1], cmap='plasma', interpolation='nearest')
+ax3.set_title('insulation = 0.9')
+
+c_6, it_6 = SOR_Iteration(1.9, coor_size=[[20,20,10]], insulation=[0.99])
+print('insulation = 0.99:', it_6)
+ax4.imshow(c_6, extent=[0, 1, 0, 1], cmap='plasma', interpolation='nearest')
+ax4.set_title('insulation = 0.99')
+
+fig.supxlabel('X')
+fig.supylabel('Y')
+fig.colorbar(first, ax=(ax1,ax2, ax3,ax4))
 plt.show()
 
-c_2, it_2 = SOR_Iteration(1.8, coor_size=[[10,10,5], [30,30,5]], insulation=[0.75, 0.25])
-print(it_2)
-plt.imshow(c_2, cmap='hot_r', interpolation='nearest')
-plt.show()
+# c_2, it_2 = SOR_Iteration(1.8, coor_size=[[10,10,5], [30,30,5]], insulation=[0.75, 0.25])
+# print(it_2)
+# plt.imshow(c_2, cmap='plasma', interpolation='nearest')
+# plt.show()
